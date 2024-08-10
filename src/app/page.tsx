8a1151a3bc05870827e2388e269e4e93bad4d4d4
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-08-11 00:38:55 +0530

    implement image upload functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default async function HomePage() {
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
+        {images.map((image, index) => (
           <div key={image.id + "-" + index} className="flex w-48 flex-col">
             <img src={image.url} alt="image" />
             <div>{image.name}</div>
