346ec9f0661a1d30a4859142d16fee25b275f616
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-08-12 01:09:05 +0530

    fix full screen issue in image dialog component

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getImage } from "~/server/queries";
+import FullPageImageView from "~/components/full-image-page";
 
-export default async function PhotoModal({
+export default function PhotoPage({
   params: { id: photoId },
 }: {
   params: { id: string };
@@ -8,11 +9,6 @@ export default async function PhotoModal({
   const idAsNumber = Number(photoId);
 
   if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo ID");
-  const image = await getImage(idAsNumber);
 
-  return (
-    <div>
-      <img src={image.url} className="w-96" alt="thumbnail image" />
-    </div>
-  );
+  return <FullPageImageView id={idAsNumber} />;
 }
