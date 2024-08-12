db467b486d22fb7eb38ca937076b4c7a50a62105
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-08-12 13:48:53 +0530

    change some homepage ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,9 +30,11 @@ export default function RootLayout({
            */
           routerConfig={extractRouterConfig(ourFileRouter)}
         />
-        <body className="flex flex-col gap-4">
-          <TopNav />
-          {children}
+        <body>
+          <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+            <TopNav />
+            <main className="overflow-y-scroll">{children}</main>
+          </div>
           {modal}
           <div id="modal-root" />
         </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,32 +15,27 @@ export default async function HomePage() {
   }
 
   return (
-    <main className="">
-      <div className="flex flex-wrap justify-center gap-4">
-        {user.userId ? (
-          images?.map((image, index) => (
-            <div
-              key={image.id + "-" + index}
-              className="flex h-48 w-48 flex-col"
-            >
-              <Link href={`/img/${image.id}`}>
-                <Image
-                  src={image.url}
-                  alt={image.name}
-                  style={{ objectFit: "contain" }}
-                  width={192}
-                  height={192}
-                />
-              </Link>
-              <div>{image.name}</div>
-            </div>
-          ))
-        ) : (
-          <div className="flex w-full justify-center text-xl font-bold">
-            Not signed in!
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {user.userId ? (
+        images?.map((image, index) => (
+          <div key={image.id + "-" + index} className="flex h-48 w-48 flex-col">
+            <Link href={`/img/${image.id}`}>
+              <Image
+                src={image.url}
+                alt={image.name}
+                style={{ objectFit: "contain" }}
+                width={192}
+                height={192}
+              />
+            </Link>
+            <div>{image.name}</div>
           </div>
-        )}
-      </div>
-    </main>
+        ))
+      ) : (
+        <div className="flex w-full justify-center text-xl font-bold">
+          Not signed in!
+        </div>
+      )}
+    </div>
   );
 }
